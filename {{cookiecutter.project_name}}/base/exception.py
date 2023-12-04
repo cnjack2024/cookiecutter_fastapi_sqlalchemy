f7e4f8795d62b065ba9e52b909de2c34926e2c81
cnjack2024 <145744739+cnjack2024@users.noreply.github.com>
2023-12-04 11:39:42 +0800

    Update exception.py

--- {{cookiecutter.project_name}}/base/exception.py
+++ {{cookiecutter.project_name}}/base/exception.py
@@ -3,11 +3,14 @@ from starlette.exceptions import HTTPException
 from starlette.status import *
 
 
-HTTP_461_TOKEN_EXPIRED = 461
-
-
 class AuthenticationError(AuthenticationError):
     def __init__(self, msg: str = "认证失败"):
+        self.msg = msg
+        self.status_code = HTTP_401_UNAUTHORIZED
+
+
+class AuthenticationExpired(AuthenticationError):
+    def __init__(self, msg: str = "认证过期"):
         self.msg = msg
         self.status_code = HTTP_401_UNAUTHORIZED
 
@@ -16,9 +19,3 @@ class AuthenticationForbidden(AuthenticationError):
     def __init__(self, msg: str = "禁止访问"):
         self.msg = msg
         self.status_code = HTTP_403_FORBIDDEN
-
-
-class AuthenticationExpired(AuthenticationError):
-    def __init__(self, msg: str = "认证过期"):
-        self.msg = msg
-        self.status_code = HTTP_461_TOKEN_EXPIRED
